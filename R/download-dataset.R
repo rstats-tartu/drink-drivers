0bb2d11c84f10467d687c1a47660a7d72b9f3807
Taavi Päll <tapa741@gmail.com>
2018-10-25 10:02:09 +0000

    downloaded dataset

---
+++ R/download-dataset.R
@@ -0,0 +1,7 @@
+
+#' Drink-drivers dataset was downloaded from Open Data Portal of Estonia https://opendata.riik.ee/en
+#' Dataset is titled in Estonian: Liiklusjärelevalve käigus avastatud süüteod and is
+#' Published by Politsei- ja Piirivalveamet. Licensed under: Creative Commons BY-SA 3.0
+
+download.file("https://opendata.smit.ee/ppa/csv/liiklusjarelevalve_3.csv", 
+              destfile = "data/liiklusjarelevalve_3.csv")
